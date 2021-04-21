8d54c204a413ab67de8649db25e497729a5bbf8f
Anksus <sankitpramod.min18@itbhu.ac.in>
2021-04-21 20:57:20 +0530

    setup

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const ticketAPI = require("../Lucky-draw-Grofers/Raffle tickets/ticketAPI");
+const eventAPI = require("../Lucky-draw-Grofers/luckyDrawEvent/eventAPI");
 require("dotenv").config();
 
 const app = express();
@@ -15,7 +17,7 @@ const uri = process.env.ATLAS_URI;
 
 mongoose.connect(
   uri,
-  { useNewUrlParser: true, useUnifiedTopology: true },
+  { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true },
   () => {
     console.log("connection with the database is established");
   }
@@ -23,6 +25,8 @@ mongoose.connect(
 
 const port = 3000 || process.env.PORT;
 
+app.use(ticketAPI);
+app.use(eventAPI);
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}!`);
 });
