d8d676a87e48c38880189ef3a872a44bb5171b07
Anksus <sankitpramod.min18@itbhu.ac.in>
2021-04-22 15:53:10 +0530

    add event api

---
+++ luckyDraw/addEventAPI.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const router = new express.Router();
+const Event = require("./eventModel");
+const moment = require("moment");
+const computeWinner = require("./computeWinner");
+
+router.post("/add-event", async (req, res) => {
+  const data = req.body;
+  const event = new Event({
+    eventName: data.eventName,
+    startsAt: moment(new Date(data.startsAt)),
+    duration: data.duration,
+    reward: data.reward,
+  });
+  const computeData = {
+    startsAt: moment(new Date(data.startsAt)),
+    duration: data.duration,
+  };
+
+  try {
+    const duplicateEvent = await Event.findOne({ eventName: data.eventName });
+    if (duplicateEvent) {
+      res.status(201).send("Event already registered! Can't register again");
+    } else {
+      await event.save();
+      computeWinner(computeData);
+      res.send("Event registered successfully");
+    }
+  } catch (e) {
+    res.status(400).send(e);
+  }
+});
+
+module.exports = router;
